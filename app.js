f1848871a8087f4cfd62105920b6027d9d066f54
Grigory <grigirij.zalizniak.salazar@gmail.com>
2018-04-30 17:43:16 +0300

    my work with mysql+nodejs+html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let express = require('express');
 let app = express();
 
 let connection = mysql.createConnection({
-    hosr: 'localhost',
+    host: 'localhost',
     user: 'root',
     password: '',
     database: 'grisha',
@@ -23,15 +23,57 @@ app.get('/list', function (req,res) {
     var str='';
     connection.query('SELECT * FROM persons', function (err, rows, fields) {
         if(!err){
+            console.log('Query was ok' );
             for(var i in rows){
-                str += rows[i].LastName + '<br>';
+                str += '<br>' + rows[i].LastName + '<a href="/del?LastName=' + rows[i].LastName + '">-</a>';/* +
+                    ' <a href="/editfrom?PersonID="' + rows[i].PersonID + '&LastName=' + rows[i].LastName + '&FirstName=' +
+                    rows[i].FirstName + '&Address=' + rows[i].Address + '&City=' + rows[i].City + ' > !</a>'; */
             };
             res.send(str);
         }
         else{
-            console.log('Error while performing query');
+            console.log('Error while performing query:' + err);
         }
 
+    });
+});
+
+app.get('/form', function (req, res) {
+    var str = '<form action="/add" method="get">' +
+        '<input type="text" name="PersonID">' +
+        '<input type="text" name="LastName">' +
+        '<input type="text" name="FirstName">' +
+        '<input type="text" name="Address">' +
+        '<input type="text" name="City">' +
+        '<input type="submit">' +
+        '</form>';
+    res.send(str);
+});
+
+app.get('/add', function (req, res) {
+    //console.log(req.query.PersonID, req.query.LastName, req.query.FirstName, req.query.Address, req.query.City);
+    connection.query('insert into persons (PersonID, LastName, FirstName, Address, City) values ('+ req.query.PersonID +' ,'+ req.query.LastName +' , '+ req.query.FirstName +', '+ req.query.Address +', '+ req.query.City +')',
+        function(err, rows, fields){
+        if(!err){
+            res.send('ready');
+        } else
+            console.log('Error while performing query');
+    })
+});
+
+app.get('/del', function (req, res) {
+    console.log('LastName:' + req.query.LastName);
+
+    var query = "delete from persons where LastName='" + req.query.LastName+"'";
+    console.info("Running: " + query);
+    connection.query(query, function (err, rows, fields) {
+        if(!err){
+            console.log('ready');
+            //
+        } else{
+            console.log('Error while performing query');
+            //res.end();
+        }
     });
 });
 
